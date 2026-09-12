45b21d3386d62f731b5ad422745b9d1d71c0a787
Javid <javid.momeni@gmail.com>
2026-09-12 07:41:36 +0200

    fix: make sitemap route static-export compatible

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [{ url: "https://signal.coreplatform.ir", lastModified: new Date(), changeFrequency: "weekly", priority: 1 }];
 }
